7a8c6a3e74f11592f718f2df9989acaa8901d357
zhanghaohao <847824708@qq.com>
2020-07-16 16:35:32 +0800

    update

--- src/test/java/test/LengthTest.java
+++ src/test/java/test/LengthTest.java
@@ -5,7 +5,8 @@ import com.toolkit.util.ByteUtil;
 public class LengthTest {
 
 	public static void main(String[] args) {
-		int n=4;
+		int n=0;
+		System.out.println(n);
 		byte[] b=ByteUtil.getL_TH(n);
 		System.out.println(ByteUtil.bytesToHexString(ByteUtil.bytesToHexString(b).getBytes()));
 	}
